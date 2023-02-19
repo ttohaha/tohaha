f1db0845e20b148d61ad09f9571a4e8be28acf5e
ttohaha <108796182+ttohaha@users.noreply.github.com>
2023-02-19 18:17:42 +0300

    hello

--- src/components/AboutCompany/AboutCompany.js
+++ src/components/AboutCompany/AboutCompany.js
@@ -6,14 +6,16 @@ function AboutCompany() {
     <div className='aboutcompany'>
         <div className='aboutcompany_h2'>О компании</div>
         <div className='aboutcompany_text_block'>
-            <div className='aboutcompany_text'>Обладаем большим опытом в области натяжных потолков, и готовы делиться им, помогая найти лучшие решения и материалы для Вас</div>
-            <div className='aboutcompany_text'>Безопасный монтаж</div>
-            <div className='aboutcompany_text'>Один из самых важных аспектов нашей работы. Используем взрывобезопасное оборудование. </div>
-            <div className='aboutcompany_text'>Экологичные материалы</div>
-            <div className='aboutcompany_text'>Работаем с проверенными и сертифицированными материалам, которые имеют  санитарно- гигиенические и пожарные сертификаты.</div>
+            <div className='aboutcompany_text'><b style={{fontWeight:'900', fontSize:'18.5px', color:'rgb(204, 204, 204)'}}>-Обладаем большим опытом</b> в области натяжных потолков, и готовы делиться им, помогая найти лучшие решения и материалы для Вас!</div>
+            <div className='aboutcompany_text'><b style={{fontWeight:'900', fontSize:'18.5px', color:'rgb(204, 204, 204)'}}>-Безопасный монтаж </b>- oдин из самых важных аспектов нашей работы. Используем взрывобезопасное оборудование</div>
+            <div className='aboutcompany_text'><b style={{fontWeight:'900', fontSize:'18.5px', color:'rgb(204, 204, 204)'}}>-Экологичные материалы</b>. Работаем с проверенными и сертифицированными материалам, которые имеют санитарно- гигиенические и пожарные сертификаты.</div>
         </div>
     </div>
   )
+
+  
+
+
 }
 
 export default AboutCompany
